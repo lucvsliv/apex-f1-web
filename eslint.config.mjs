c03f1a9d6767b8c433a24a2903ed2547ec53ff05
gongjuheon <yh6543210@gmail.com>
2026-03-22 23:02:47 +0900

    fix: [APEX-61] @eslint/eslintrc import 관련 오류 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 
 export default [
-    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("next/core-web-vitals"),
 
     {
         ignores: [
