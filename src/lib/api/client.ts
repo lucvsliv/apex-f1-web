bb0d027d6e898777a090d40b0299151adf779e9a
gongjuheon <yh6543210@gmail.com>
2026-05-01 04:24:50 +0900

    fix: [APEX-78] 도커 이미지 최신화 오류 수정

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// 1. 기본 인스턴스 생성
+// 1. 기본 Instance 생성
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api/v1",
     headers: {
